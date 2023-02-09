6d44a379ee6625c8f9cbc7050e0a08180cf4febb
22050039-Yinhua Chen <12132329@mail.sustech.edu.cn>
2023-02-09 11:16:10 +0800

    update example use_FIFO_channel

--- examples/use_FIFO_channel/source.h
+++ examples/use_FIFO_channel/source.h
@@ -8,6 +8,9 @@ class source : public sc_module
   public:
   sc_in_clk clk;
   sc_port<sc_fifo_out_if<char> > write_port;    
+  // 类sc_port<IF,N>是所有端口的基类，它是一个模板类。
+  // IF是接口类型，N是所连接的同一类型的通道数目，也就
+  // 是接口数，它的缺省值是1。
   SC_HAS_PROCESS(source);
 
   source(sc_module_name name) : sc_module(name) {
